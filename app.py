8a005b736587c0a83fe5cee97482224def434d1a
Lucas Gelenski <gelenskilucas@gmail.com>
2024-04-10 13:53:00 -0300

    adicionando funções e main

--- app.py
+++ app.py
@@ -6,21 +6,38 @@ def sair():
     print("Finalizando aplicação.")
 
 
-print("Programa Expresso")
-print("1 - Cadastrar restaurante")
-print("2 - Listar restaurante")
-print("3 - Ativar restaurante")
-print("4 - Sair")
-opcao_digitada = int(input("Selecione uma opção:\n"))
-print("\nVocê escolheu a opção:", opcao_digitada)
-match opcao_digitada:
-    case 1:
-        print("Você escolheu cadastrar seu restaurante.")
-    case 2:
-        print("Você escolheu listar seu restaurante.")
-    case 3:
-        print("Você escolheu ativar seu restaurante.")
-    case 4:
-        print("Você escolheu sair.")
-    case _:
-        print("Escolha uma opção valida.")
+def chama_nome_app():
+    print("Programa Expresso")
+
+
+def lista_opcoes():
+    print("1 - Cadastrar restaurante")
+    print("2 - Listar restaurante")
+    print("3 - Ativar restaurante")
+    print("4 - Sair")
+
+
+def escolha_opcoes():
+    opcao_digitada = int(input("Selecione uma opção:\n"))
+    print("\nVocê escolheu a opção:", opcao_digitada)
+    match opcao_digitada:
+        case 1:
+            print("Você escolheu cadastrar seu restaurante.")
+        case 2:
+            print("Você escolheu listar seu restaurante.")
+        case 3:
+            print("Você escolheu ativar seu restaurante.")
+        case 4:
+            sair()
+        case _:
+            print("Escolha uma opção valida.")
+
+
+def main():
+    chama_nome_app()
+    lista_opcoes()
+    escolha_opcoes()
+
+
+if __name__ == "__main__":
+    main()
